0e6509263ec8f7905522e5b719888d4961b7f956
Cyril <cyril.fleurot@gmail.com>
2024-10-23 16:16:06 +1300

    Change data structure and add entries

--- main.py
+++ main.py
@@ -3,9 +3,30 @@ import json
 def display_data():
     with open('data.json', 'r') as file:
         data = json.load(file)
-        print(f"Name: {data['name']}")
-        print(f"Age: {data['age']}")
-        print(f"City: {data['city']}")
+        for entry in data:
+            print(f"Name: {entry['name']}")
+            print(f"Age: {entry['age']}")
+            print(f"City: {entry['city']}")
+            print('---')
+
+def add_data(data):
+    with open('data.json', 'r') as file:
+        old_data = json.load(file)
+    old_data.append(data)
+    with open('data.json', 'w') as file:
+        json.dump(old_data, file, indent=4)
+
+def enter_data():
+    name = input("Enter name: ")
+    age = input("Enter age: ")
+    city = input("Enter city: ")
+    data = {
+        'name': name,
+        'age': age,
+        'city': city
+    }
+    add_data(data)
 
 if __name__ == "__main__":
     display_data()
+    enter_data()
